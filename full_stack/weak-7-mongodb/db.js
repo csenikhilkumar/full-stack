2c7ebc0ee52cb3d84e8e0e0757f6032fb2f79489
nikhil0277 <nikhil07407@gmail.com>
2024-12-10 23:36:14 +0530

    8 dec's commit

---
+++ full_stack/weak-7-mongodb/db.js
@@ -0,0 +1,24 @@
+const mongoose =require ("mongoose");
+const userId =mongoose.userId
+// const { String, boolean } = require("zod");
+const schema =mongoose.Schema;
+const user = new schema({
+    email:String,
+    password:String,
+    username:String
+})
+
+const todo =new schema({
+    description:String,
+    done:Boolean,
+    // userId:userId
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true } 
+})
+
+const usermodel = mongoose.model('users',user)
+const todomodel= mongoose.model('todos',todo)
+
+module.exports={
+    usermodel:usermodel,
+    todomodel:todomodel
+}
---
+++ full_stack/weak-7-mongodb/index.js
@@ -0,0 +1,88 @@
+const express =require("express")
+const jwt =require("jsonwebtoken");
+const mongoose=require("mongoose")
+const jwt_secrete ="helloiam"
+mongoose.connect("mongodb+srv://admin:7fegnJGsBAVLeCoh@cluster0.t9tqw.mongodb.net/users-1626")
+const app = express();
+
+const {usermodel,todomodel}=require("./db");
+
+app.use(express.json())
+function auth(req,res,next){
+    const token =req.headers.authorization
+    const response =jwt.verify(token,jwt_secrete)
+    if(response){
+        req.userId=response.id
+        next()
+    }
+    else{
+        res.status(403).json({
+            massage:"anauthorized access"
+        })
+    }
+}
+
+app.post("/signup",async function(req,res){
+    const email =req.body.email;
+    const password=req.body.password;
+    const username =req.body.username;
+    await usermodel.create({
+        email:email,
+        password:password,
+        username:username
+    })
+
+    res.json({
+        "massage":"you are signed-up"
+    })
+
+})
+
+app.post("/signin",async function(req,res){
+    const email =req.body.email;
+    const password=req.body.password;
+    const username =req.body.username;
+    const user = await usermodel.findOne({
+        email:email,
+        password:password,
+        usrname:username
+    })
+
+    if(user){
+        console.log({
+            id:user._id.toString()
+        })
+        const token =jwt.sign({
+            id:user._id.toString()
+        },jwt_secrete)
+        res.json({
+           token:token
+        })
+    }
+    else{
+        res.json({
+            "masssge":"invalid cridentials"
+        })
+    }
+
+})
+
+app.post("/todo",auth,function(req,res){
+
+    const userId=req.userId
+    res.json({
+        userId:userId
+    })
+
+})
+
+app.get("/todos",auth,function(req,res){
+    const userId=req.userId
+    res.json({
+        userId:userId
+    })
+})
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000")
+})
