b847d6b48e4c9afb30b7438246af584f7f3ee0d4
csenikhilkumar <nikhil07407@gamil.com>
2025-03-13 20:12:30 +0530

    i will again complete grid and flexbox for my practice purpose

---
+++ full_stack/weak-12_again-tailwind/flexbox_&_grids/again_css/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-12">
+        <div className="bg-blue-200 col-span-6">hi my name is nikhil</div>
+        <div className="bg-red-200 col-span-4">hello</div>
+        <div className="bg-yellow-200 col-span-2">hi what are you doing</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
